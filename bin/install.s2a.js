3020639ef70e2b67ac82c904c38c2eb8e673e36c
Khurram Ali <khurram.ali@riksof.com>
2023-11-28 19:12:45 +0500

    Update install script for new sdk

--- bin/install.s2a.js
+++ bin/install.s2a.js
@@ -15,7 +15,6 @@ const mobile = path.join( process.env.S2A_PATH, 'dist', 'mobile' );
 const desktop = path.join( process.env.S2A_PATH, 'dist', 'desktop' );
 const jit = path.join( process.env.S2A_PATH, 'dist', 'jit' );
 const loaders = path.join( process.env.S2A_PATH, 'compiler' );
-const schema = path.join( process.env.S2A_PATH, 'schema' );
 
 // Important destinations
 const assetsPkg = path.join( __dirname, '..', 'node_modules', '@s2a', 'app-assets' );
@@ -24,7 +23,6 @@ const mobilePkg = path.join( __dirname, '..', 'node_modules', '@s2a', 'mobile' )
 const desktopPkg = path.join( __dirname, '..', 'node_modules', '@s2a', 'desktop' );
 const jitPkg = path.join( __dirname, '..', 'node_modules', 'jit' );
 const loadersPkg = path.join( __dirname, '..', 'node_modules', '@s2a', 'compiler' );
-const schemaPkg = path.join( __dirname, '..', 'node_modules', '@s2a', 'schema' );
 
 // First ensure the parent directory exists.
 fs.ensureDirSync( path.join( __dirname, '..', 'node_modules', '@s2a' ) );
@@ -67,7 +65,6 @@ updateLink( jitPkg, jit );
 
 // Only the compiler loaders have to be copied.
 fs.copy( loaders, loadersPkg );
-fs.copy( schema, schemaPkg );
 
 // Give feedback to user.
 console.log( '@s2a client packages are installed and ready for use.' );
